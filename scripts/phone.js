a03bbb6e9b6748b972f18cca0bb6c13e60ad5046
Sayem <sayem465508@gmail.com>
2024-02-28 23:09:44 +0600

    added show all btn and show first 12 phone in a row

--- scripts/phone.js
+++ scripts/phone.js
@@ -1,5 +1,5 @@
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones =data.data;
     displayPhone(phones)
@@ -7,6 +7,23 @@ const loadPhone = async () => {
 
 const displayPhone = (phones) => {
     const phoneContainer = document.getElementById('phone-container')
+
+    // clear phoneContainer before adding phoneCard
+    phoneContainer.textContent = '';
+
+    // display show all btn if there are more than 12 phone
+    const showAllBtn = document.getElementById('show-all-btn');
+    if(phones.length > 12){
+        showAllBtn.classList.remove('hidden')
+    }
+    else{
+        showAllBtn.classList.add('hidden')
+    }
+    
+    // show first 12 phone 
+    phones = phones.slice(0, 12)
+
+
     phones.forEach((phone) => {
         const {brand, phone_name, slug, image} = phone;
         console.log(phone)
@@ -27,5 +44,12 @@ const displayPhone = (phones) => {
     })
 }
 
+// handle search button
+const handleSearch = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhone(searchText);
+}
 
-loadPhone()
+
+// loadPhone()
