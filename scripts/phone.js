b25bfbba0bf09cc883e764b2e199431772e058c9
Sayem <sayem465508@gmail.com>
2024-02-29 08:04:39 +0600

    added loading spinner

--- scripts/phone.js
+++ scripts/phone.js
@@ -42,14 +42,27 @@ const displayPhone = (phones) => {
         `;
         phoneContainer.appendChild(phoneCard)
     })
+    // hide loading spinner
+    toggleLoadingSpinner(false)
 }
 
 // handle search button
 const handleSearch = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
+    toggleLoadingSpinner(true);
     loadPhone(searchText);
 }
 
+
+const toggleLoadingSpinner = (isLoading) => {
+    const loadingSpinner = document.getElementById('loading-spinner');
+    if(isLoading){
+        loadingSpinner.classList.remove('hidden');
+    }
+    else{
+        loadingSpinner.classList.add('hidden')
+    }
+}
 
 // loadPhone()
