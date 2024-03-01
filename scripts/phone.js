10dacb7c661a38610f3a885135f59773c458ca82
Sayem <sayem465508@gmail.com>
2024-03-01 19:33:32 +0600

    dynamically loaded the data for phone details

--- scripts/phone.js
+++ scripts/phone.js
@@ -39,7 +39,7 @@ const displayPhone = (phones, isShowAll) => {
             <h2 class="card-title">${phone_name}</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div class="card-actions justify-end">
-                <button class="btn btn-primary">Show Details</button>
+                <button onclick="showDetails('${slug}')" class="btn btn-primary">Show Details</button>
             </div>
             </div>
         `;
@@ -70,6 +70,14 @@ const toggleLoadingSpinner = (isLoading) => {
 
 const handleShowAll = () => {
     handleSearch(true)
+}
+
+// show details
+const showDetails = async (id) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const phoneDetails = data.data;
+    console.log(phoneDetails)
 }
 
 // loadPhone()
